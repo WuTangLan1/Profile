6191592aa0218a99b72871b3e1023b5762085b7d
WuTangLan1 <finnmassari16@gmail.com>
2024-08-24 14:56:01 +0200

    fix in file format -> merge

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,16 @@
 // vue.config.js
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: config => {
-    // Clear existing rules and use asset modules
     config.module.rules.delete('images');
+
     config.module
       .rule('assets')
-      .test(/\.(pdf|png|jpe?g|gif|svg|webp)$/i)
+      .test(/\.(pdf|png|jpeg|jpg|gif|svg|webp)$/i) 
       .type('asset/resource')
-      .use('asset-resource')
-        .loader('asset-resource')
-        .options({
-          name: 'assets/[name].[hash:8].[ext]'
-        });
+      .set('generator', {
+        filename: 'assets/[name].[hash:8][ext]' 
+      });
   }
-})
+});
